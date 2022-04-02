6c98e100464315f74ff18fbf66fce138e1add4b0
Nicolas Ramos Marin <nicorm2000@gmail.com>
2022-04-02 00:23:05 -0300

    primer commit

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    [SerializeField] private float rotationSpeed;
+    private float horizontal;
+    private float vertical;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+        transform.position += new Vector3(horizontal, vertical, 0) * Time.deltaTime * velocity;
+        PlayerRotation();
+    }
+
+    void PlayerMovement()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void PlayerRotation()
+    {
+        Vector2 movementDirection = new Vector2(horizontal, vertical);
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
